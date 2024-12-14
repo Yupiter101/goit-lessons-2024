be6afc95c9ccfce04f76cabbcc3ac9e433b91b67
Sergey_Sanyuk <shv08@ukr.net>
2024-12-14 12:48:26 +0200

    promise

--- js/promise.js
+++ js/promise.js
@@ -3,6 +3,9 @@ console.log("It is promise");
 import { randomColor } from "../js/admins.js";
 
 
+
+// ===== setTimeout() ===============
+
 const btnTimout = document.querySelector(".btn-timout");
 const field = document.querySelector(".field");
 
@@ -18,3 +21,30 @@ const onClick = () => {
 
 
 btnTimout.addEventListener("click", onClick);
+
+
+
+// ===== setInterval() ===============
+
+const startBtn = document.querySelector(".btn-start");
+const stopBtn = document.querySelector(".btn-stop");
+const result = document.querySelector(".interval-res");
+
+let timerId = null;
+let counterTime = 0;
+
+const onStart = () => {
+    console.log("Start");
+    timerId = setInterval(()=> {
+        counterTime += 1;
+        result.textContent = counterTime;
+    }, 100);
+}
+startBtn.addEventListener("click", onStart );
+
+
+const onStop = () => {
+    clearInterval(timerId); //
+    console.log("Stop");
+}
+stopBtn.addEventListener("click", onStop );
